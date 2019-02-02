7f2cf8d4f141edf106d971b1b74a50548964f462
Dominic Philip <dominic@Doms-MacBook-Pro-2.local>
2019-02-02 18:54:40 -0500

    update arduino cmake and add libs

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-
-void setup() {
-    // Setup
-}
-
-void loop() {
-    // Loop
-}
---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+#include <Adafruit_BNO055.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_VL53L0X.h>
+
+void setup() {
+    // Setup
+}
+
+void loop() {
+    // Loop
+}
