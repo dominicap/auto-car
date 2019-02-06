ea314bc9b23f2740946f1752dade60e662bd1d71
Dominic Philip <domi.a.philip@gmail.com>
2019-02-06 14:03:35 -0500

    unveil errors

--- src/auto_car.cpp
+++ src/auto_car.cpp
@@ -6,15 +6,23 @@
 
 #define BAUD_RATE 9600
 
+Adafruit_BNO055 imuSensor = Adafruit_BNO055(55);
+Adafruit_VL53L0X lidarSensor = Adafruit_VL53L0X();
+
 void setup()
 {
     Serial.begin(BAUD_RATE);
+
+    if (!lidarSensor.begin()) {
+        Serial.println("Error: Lidar sensor is not setup properly.");
+    }
+
+    if (!imuSensor.begin()) {
+        Serial.println("Error: IMU sensor is not setup properly.");
+    }
 }
 
 void loop()
 {
-    if (Serial.available()) {
-        Serial.println("Hello, World!");
-        delay(2000);
-    }
+    // Loop
 }
